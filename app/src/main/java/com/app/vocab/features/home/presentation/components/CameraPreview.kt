f8c8733d08fc6a160fd4c9df7570d682ae870d55
Jemish Khunt <jemish.k@crestinfosystems.com>
2024-12-16 19:14:24 +0530

    fixed home screen

--- app/src/main/java/com/app/vocab/features/home/presentation/components/CameraPreview.kt
+++ app/src/main/java/com/app/vocab/features/home/presentation/components/CameraPreview.kt
@@ -7,12 +7,17 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -31,46 +36,50 @@ fun CameraPreview(captureImage: (Bitmap) -> Unit) {
     val previewView = remember { PreviewView(context) }
     val imageCapture = remember { ImageCapture.Builder().build() }
 
-    AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize()) {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
-        cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder().build().also { preview ->
-                preview.surfaceProvider = previewView.surfaceProvider
-            }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-            cameraProvider.unbindAll()
-            try {
-                cameraProvider.bindToLifecycle(
-                    lifecycleOwner,
-                    cameraSelector,
-                    preview,
-                    imageCapture
-                )
-            } catch (e: Exception) {
-                Log.e("CameraPreview", "Failed to show CameraPreview")
-            }
-        }, ContextCompat.getMainExecutor(context))
-    }
-
-    // Capture Button
     Box(
         contentAlignment = Alignment.BottomCenter,
-        modifier = Modifier.fillMaxSize()
     ) {
-        Button(
-            onClick = {
-                previewView.bitmap?.let { captureImage(it) }
-                previewView.bitmap ?: {
-                    Log.e("CameraPreview", "CameraPreview: Failed to get Bitmap")
+        AndroidView(factory = { previewView }) {
+            val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
+            cameraProviderFuture.addListener({
+                val cameraProvider = cameraProviderFuture.get()
+                val preview = Preview.Builder().build().also { preview ->
+                    preview.surfaceProvider = previewView.surfaceProvider
                 }
-            },
-            modifier = Modifier
-                .padding(16.dp)
-                .clip(CircleShape)
-        ) {
-            Text("Capture")
+
+                val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+                cameraProvider.unbindAll()
+                try {
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner,
+                        cameraSelector,
+                        preview,
+                        imageCapture
+                    )
+                } catch (e: Exception) {
+                    Log.e("CameraPreview", "Failed to show CameraPreview")
+                }
+            }, ContextCompat.getMainExecutor(context))
         }
+
+        Image(
+            imageVector = Icons.Default.PhotoCamera,
+            contentDescription = "Capture image",
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .size(60.dp)
+                .clip(CircleShape)
+                .background(MaterialTheme.colorScheme.primary)
+                .padding(12.dp)
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ) {
+                    previewView.bitmap?.let { captureImage(it) }
+                    previewView.bitmap ?: {
+                        Log.e("CameraPreview", "CameraPreview: Failed to get Bitmap")
+                    }
+                }
+        )
     }
 }
--- app/src/main/java/com/app/vocab/features/home/presentation/screen/HomeScreen.kt
+++ app/src/main/java/com/app/vocab/features/home/presentation/screen/HomeScreen.kt
@@ -1,15 +1,16 @@
 package com.app.vocab.features.home.presentation.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,8 +24,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.app.vocab.R
 import com.app.vocab.features.home.domain.state.SaveImageState
 import com.app.vocab.features.home.presentation.components.CameraPreview
 import com.app.vocab.features.home.presentation.components.RationaleDialog
@@ -78,12 +82,17 @@ fun HomeScreen(
         modifier = Modifier
             .padding(innerPadding)
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-            .verticalScroll(rememberScrollState())
-            .padding(20.dp),
+            .background(MaterialTheme.colorScheme.background),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom
     ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_forget_pass_alert),
+                contentDescription = ""
+            )
+            Text(text = "Vocab", fontSize = 40.sp)
+        }
         when {
             cameraPermissionState.status.isGranted -> {
                 CameraPreview(captureImage = { homeViewModel.saveImage(it) })
@@ -109,41 +118,36 @@ fun HomeScreen(
             }
 
             else -> {
-                PermissionRequiredText(
-                    onRequestPermission = {
-                        if (!cameraPermissionState.status.isGranted) {
-                            handlePermissionRequest(
-                                cameraPermissionState = cameraPermissionState,
-                                hasRequested = hasRequestedPermission,
-                                onHasRequested = {
-                                    hasRequestedPermission = it
-                                },
-                                onResult = { rationale, settings ->
-                                    showRationaleDialog = rationale
-                                    showSettingsDialog = settings
-                                }
-                            )
-                        } else {
-                            cameraPermissionState.launchPermissionRequest()
+                Column(
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text("Camera permission is required to use this feature.")
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Button(
+                        onClick = {
+                            if (!cameraPermissionState.status.isGranted) {
+                                handlePermissionRequest(
+                                    cameraPermissionState = cameraPermissionState,
+                                    hasRequested = hasRequestedPermission,
+                                    onHasRequested = {
+                                        hasRequestedPermission = it
+                                    },
+                                    onResult = { rationale, settings ->
+                                        showRationaleDialog = rationale
+                                        showSettingsDialog = settings
+                                    }
+                                )
+                            } else {
+                                cameraPermissionState.launchPermissionRequest()
+                            }
                         }
+                    ) {
+                        Text("Grant Permission")
                     }
-                )
+                }
             }
-        }
-    }
-}
-
-@Composable
-fun PermissionRequiredText(onRequestPermission: () -> Unit) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text("Camera permission is required to use this feature.")
-        Spacer(modifier = Modifier.height(8.dp))
-        Button(onClick = onRequestPermission) {
-            Text("Grant Permission")
         }
     }
 }
